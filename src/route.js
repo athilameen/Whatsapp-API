7ae0e931c90da9cc70e10732264aece20aab012f
Athil Ameen <athilammeen@gmail.com>
2024-09-05 16:37:37 +0530

    api update 004

--- src/route.js
+++ src/route.js
@@ -8,8 +8,25 @@ const router = express.Router();
 //router.get("/whatsapp", whatsappController.VerifyToken);
 //router.post("/whatsapp", whatsappController.ReceivedMessage);
 
-router.get("/twhatsapp", whatsappController.TVerifyToken)
-router.post("/twhatsapp", whatsappController.TReceivedMessage);
+//router.get("/twhatsapp", whatsappController.TVerifyToken)
+//router.post("/twhatsapp", whatsappController.TReceivedMessage);
+
+router.get("/log", (req, res) => {
+    
+    // Path to the text file
+    const filePath = '../log.txt';
+
+    // Read the file
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading the file', err);
+            return res.status(500).send('Failed to read the file');
+        }
+        // Send the file content as a response
+        res.send(data);
+    });
+
+});
 
 router.get("/health", (req, res) => {
     const healthInfo = {
