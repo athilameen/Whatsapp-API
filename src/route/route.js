bff0ef26cf47b942dc93a242d10edee31ad345b9
Athil Ameen <athilammeen@gmail.com>
2024-09-05 13:24:16 +0530

    Log path added

--- src/route/route.js
+++ src/route/route.js
@@ -1,7 +1,25 @@
 const app = require("express");
+const fs = require('fs');
 const router = app.Router();
 const whatsappController = require("../controllers/WhatsappControllers")
 
-router.get("/", whatsappController.VerifyToken).post("/", whatsappController.ReceivedMessage);
+router.get("/whatsapp", whatsappController.VerifyToken).post("/whatsapp", whatsappController.ReceivedMessage);
+
+router.get("/log", (req, res) => {
+    
+    // Path to the text file
+    const filePath = './log.txt';
+
+    // Read the file
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading the file', err);
+            return res.status(500).send('Failed to read the file');
+        }
+        // Send the file content as a response
+        res.send(data);
+    });
+
+});
 
 module.exports = router;
